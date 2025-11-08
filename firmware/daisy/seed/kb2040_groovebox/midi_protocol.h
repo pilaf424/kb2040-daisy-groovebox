ee743df4fd6c24f89943867265d6b2adb7fd52b9
pilaf424 <philliplopez424@gmail.com>
2025-11-08 11:15:16 -0500

    Add MIDI protocol version metadata

--- firmware/daisy/seed/kb2040_groovebox/midi_protocol.h
+++ firmware/daisy/seed/kb2040_groovebox/midi_protocol.h
@@ -3,3 +3,6 @@
 // Wrapper to keep legacy include paths alive for the Daisy firmware.
 // Pulls in the canonical MIDI definitions shared across both targets.
 #include "../../../midi_protocol.h"
+
+static_assert(MIDI_PROTOCOL_VERSION == 0x0001'0002,
+              "Daisy firmware built with mismatched MIDI protocol");
--- firmware/kb2040/arduino/kb2040_groovebox_ui/midi_protocol.h
+++ firmware/kb2040/arduino/kb2040_groovebox_ui/midi_protocol.h
@@ -3,3 +3,6 @@
 // Wrapper to keep the Arduino sketch's include path stable.
 // Includes the shared MIDI protocol definitions for both targets.
 #include "../../../midi_protocol.h"
+
+static_assert(MIDI_PROTOCOL_VERSION == 0x0001'0002,
+              "KB2040 UI built with mismatched MIDI protocol");
